49292d85d1d3aebb7d00b3c00c84993e1ff808a4
awr7 <147633136+awr7@users.noreply.github.com>
2024-05-23 14:48:08 -0400

    initial commit

---
+++ backend/student-loan-management/src/main/java/com/angelrosario/student_loan_management/StudentLoanManagementApplication.java
@@ -0,0 +1,13 @@
+package com.angelrosario.student_loan_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLoanManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLoanManagementApplication.class, args);
+	}
+
+}
---
+++ backend/student-loan-management/src/test/java/com/angelrosario/student_loan_management/StudentLoanManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelrosario.student_loan_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLoanManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
